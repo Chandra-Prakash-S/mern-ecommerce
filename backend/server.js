81d9203f5ef8974b1bb9bb96cbb8d6a7b1eff919
Chandra-Prakash-S <cprakashm11@gmail.com>
2026-04-21 22:53:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
@@ -17,7 +18,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -32,8 +34,8 @@ app.use("/api/analytics", analyticsRoutes)
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+	app.get("/*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 	});
 }
 
